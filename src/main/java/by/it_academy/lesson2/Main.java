93fafab96927b353da0c8f7d7fb2d4f51c097c54
smira12 <m1ronsa89@gmail.com>
2022-05-30 00:08:55 +0300

    lesson2 task 1 modification

--- src/main/java/by/it_academy/lesson2/Main.java
+++ src/main/java/by/it_academy/lesson2/Main.java
@@ -2,17 +2,17 @@ package by.it_academy.lesson2;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "Miron";
-        int mironAge = 32;
-        float mironHeight = 1.70f;
+        String name = "Sergey";
+        int sergeyAge = 32;
+        float sergeyHeight = 1.70f;
         int mass = 50;
         int result = 1 + 2;
         double pi = 3.14;
         pi = pi % 3;
 
         System.out.println(name);
-        System.out.println(mironAge);
-        System.out.println(mironHeight);
+        System.out.println(sergeyAge);
+        System.out.println(sergeyHeight);
         System.out.println(mass);
         System.out.println("1+2=" + result);
         System.out.println(pi);
