a7b29ba08208030a181e38263080ce1294ea8655
smira12 <m1ronsa89@gmail.com>
2022-06-23 19:25:41 +0300

    lesson7 correct FlipLine

--- src/main/java/by/it_academy/lesson7/FlipLine.java
+++ src/main/java/by/it_academy/lesson7/FlipLine.java
@@ -3,10 +3,10 @@ package by.it_academy.lesson7;
 public class FlipLine {
     public static void main(String[] args) {
         String str = "some string";
-        String[] words = new StringBuilder(str).reverse().toString().split("some string");
         StringBuilder newStr = new StringBuilder();
-        for (int i = words.length - 1; i >= 0; i--)
-            newStr.append(words[i]).append(" ");
+        for (int i = str.length() - 1; i >= 0; i--) {
+            newStr.append(str.charAt(i));
+        }
         System.out.println(newStr);
     }
 }
