e5b4bc0ef55080f96887a33296af9726204a8de6
smira12 <m1ronsa89@gmail.com>
2022-06-26 23:45:48 +0300

    lesson7 Task1 correct

--- src/main/java/by/it_academy/lesson7/Task1.java
+++ src/main/java/by/it_academy/lesson7/Task1.java
@@ -6,12 +6,11 @@ public class Task1 {
         int s = 0;
         int year = 0;
         String maxName = null;
-        int max = 0;
         for (int i = 0; i < array.length; i++) {
             String[] arrayOfPeople = array[i].split(",");
             s += Integer.parseInt(arrayOfPeople[1]);
-            if (arrayOfPeople[0].length()>max) {
-                max = arrayOfPeople[0].length();
+            if (arrayOfPeople[0].length()>year) {
+                year = arrayOfPeople[0].length();
                 maxName = arrayOfPeople[0];
             }
         }
