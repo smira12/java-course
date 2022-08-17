e3af0b4f26440880f95fbfd5062a6cf303ce126a
smira12 <m1ronsa89@gmail.com>
2022-08-17 09:26:52 +0300

    lesson 15

---
+++ src/main/java/by/it_academy/lesson15/Factorial.java
@@ -0,0 +1,21 @@
+package by.it_academy.lesson15;
+
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.LongStream;
+
+public class Factorial {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newWorkStealingPool();
+        Future<BigInteger> future = service.submit(() -> LongStream.rangeClosed(1, 100000)
+                .parallel()
+                .mapToObj(BigInteger::valueOf)
+                .reduce(BigInteger.ONE, BigInteger::multiply));
+
+        System.out.println(new DecimalFormat("0.###E0").format(future.get()));
+    }
+}
